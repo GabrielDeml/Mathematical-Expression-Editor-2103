fe9986594826613fb0fee7d319b48c16c28ea754
GregoryConrad <gregorysconrad@gmail.com>
2019-12-13 16:48:53 -0500

    Updated ExpressionEditor

--- src/ExpressionEditor.java
+++ src/ExpressionEditor.java
@@ -1,7 +1,5 @@
 import javafx.application.Application;
-
 import javafx.event.EventHandler;
-import javafx.geometry.Bounds;
 import javafx.geometry.Point2D;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
@@ -13,39 +11,25 @@ import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 public class ExpressionEditor extends Application {
-    public static void main(String[] args) {
-        launch(args);
-    }
+    /**
+     * Currently focused Expression
+     */
+    private Expression focusExpression;
 
+    /**
+     * The root Expression
+     */
+    private Expression rootExpression;
 
+    /**
+     * The Pane of the window
+     */
+    private Pane pane;
 
     /**
      * Mouse event handler for the entire pane that constitutes the ExpressionEditor
      */
-    private static class MouseEventHandler implements EventHandler<MouseEvent> {
-
-        /**
-         * Currently focused Expression
-         */
-        //TODO this line
-        private Expression focusExpression;
-
-        /**
-         * rootExpression
-         */
-        CompoundExpression rootExpression;
-
-        /**
-         * pane
-         */
-        Pane pane;
-
-        MouseEventHandler(Pane pane_, CompoundExpression rootExpression_) {
-            this.rootExpression = rootExpression_;
-            this.focusExpression = rootExpression_;
-            this.pane = pane_;
-        }
-
+    private class MouseEventHandler implements EventHandler<MouseEvent> {
         /**
          * Finds the clicked expression
          *
@@ -59,59 +43,38 @@ public class ExpressionEditor extends Application {
                 // Get the width and height of the hbox
                 double tmpWidth = expression.getNode().getLayoutBounds().getWidth();
                 double tmpHeight = expression.getNode().getLayoutBounds().getHeight();
-                Point2D tmpSceneLocation = expression.getNode().localToScene(expression.getNode().getBoundsInLocal().getMinX(), expression.getNode().getBoundsInLocal().getMinY());
+                Point2D tmpSceneLocation = expression.getNode()
+                        .localToScene(expression.getNode().getBoundsInLocal().getMinX(),
+                                expression.getNode().getBoundsInLocal().getMinY());
                 // Check if we are in those bounds
-                if (tmpSceneLocation.getX() < X && X < (tmpSceneLocation.getX() + tmpWidth) && tmpSceneLocation.getY() < Y && Y < (tmpSceneLocation.getY() + tmpHeight)) {
+                if (tmpSceneLocation.getX() < X && X < (tmpSceneLocation.getX() + tmpWidth) &&
+                        tmpSceneLocation.getY() < Y && Y < (tmpSceneLocation.getY() + tmpHeight)) {
                     return expression;
                 }
             }
             return null;
         }
 
-        /**
-         * Sets the border
-         * @param expToFind
-         */
-        private void setBoarder(Expression expToFind){
-            //TODO Work on this rootExpression
-            if(rootExpression instanceof LiteralExpression){
-                expToFind.getNode().setStyle("-fx-border-style: solid inside;"
-                        + "-fx-border-width: 1;" + "-fx-border-insets: 2;"
-                        + "-fx-border-color: red;");
-            }
-            else {
-                CompoundExpressionAb currentFocusCompound = (CompoundExpressionAb) rootExpression;
-                for (Expression expression : currentFocusCompound.getChildren()) {
-                    if (expression == expToFind) {
-                        expression.getNode().setStyle("-fx-border-style: solid inside;"
-                                + "-fx-border-width: 1;" + "-fx-border-insets: 2;"
-                                + "-fx-border-color: red;");
-                    } else expression.getNode().setStyle(null);
-                }
-            }
-        }
-
-
         double mouseLocationOnClickX, mouseLocationOnClickY;
 //        double orgTranslateX, orgTranslateY;
 
         /**
          * This is called when there is a mouse event
+         *
          * @param event
          */
         public void handle(MouseEvent event) {
             if (event.getEventType() == MouseEvent.MOUSE_PRESSED) {
-                if(focusExpression == null){
+                if (focusExpression == null) {
                     focusExpression = rootExpression;
                 }
                 mouseLocationOnClickX = event.getSceneX();
                 mouseLocationOnClickY = event.getSceneY();
                 Expression tmpExpression = findClickedExpression(mouseLocationOnClickX, mouseLocationOnClickY);
 
-
                 if (tmpExpression != null) {
                     focusExpression = tmpExpression;
-                    setBoarder(tmpExpression);
+                    setBorder(tmpExpression);
 //                    orgTranslateX = ((Pane) (event.getSource())).getTranslateX();
 //                    orgTranslateY = ((Pane) (event.getSource())).getTranslateY();
                 }
@@ -148,57 +111,79 @@ public class ExpressionEditor extends Application {
      */
     private final ExpressionParser expressionParser = new SimpleExpressionParser();
 
+    public static void main(String[] args) {
+        launch(args);
+    }
+
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Expression Editor");
+        pane = new Pane();
 
         // Add the textbox and Parser button
         final Pane queryPane = new HBox();
         final TextField textField = new TextField(EXAMPLE_EXPRESSION);
-        final Button button = new Button("Parse");
         queryPane.getChildren().add(textField);
+        final Button button = new Button("Parse");
+        button.setOnMouseClicked(e -> {
+            pane.getChildren().clear();
+            // Try to parse the expression
+            try {
+                // Success! Add the expression's Node to the pane
+                rootExpression = expressionParser.parse(textField.getText(), true);
+                System.out.println(rootExpression.convertToString());
+                pane.getChildren().add(rootExpression.getNode());
+                // fixme What is the purpose of these next two lines? Shouldn't you center in window instead?
+                rootExpression.getNode().setLayoutX((double) WINDOW_WIDTH / 4);
+                rootExpression.getNode().setLayoutY((double) WINDOW_HEIGHT / 2);
 
-        final Pane expressionPane = new Pane();
-
-        // Add the callback to handle when the Parse button is pressed
-        button.setOnMouseClicked(new EventHandler<MouseEvent>() {
-            public void handle(MouseEvent e) {
-                // Try to parse the expression
-                try {
-                    // Success! Add the expression's Node to the expressionPane
-                    final Expression expression = expressionParser.parse(textField.getText(), true);
-                    System.out.println(expression.convertToString(0));
-                    expressionPane.getChildren().clear();
-                    expressionPane.getChildren().add(expression.getNode());
-                    expression.getNode().setLayoutX(WINDOW_WIDTH / 4);
-                    expression.getNode().setLayoutY(WINDOW_HEIGHT / 2);
-
-                    // If the parsed expression is a CompoundExpression, then register some callbacks
-                    if (expression instanceof CompoundExpression) {
-                        ((Pane) expression.getNode()).setBorder(Expression.NO_BORDER);
-                        final MouseEventHandler eventHandler = new MouseEventHandler(expressionPane, (CompoundExpression) expression);
-                        expressionPane.setOnMousePressed(eventHandler);
-                        expressionPane.setOnMouseDragged(eventHandler);
-                        expressionPane.setOnMouseReleased(eventHandler);
-                    }
-                } catch (ExpressionParseException epe) {
-                    // If we can't parse the expression, then mark it in red
-                    textField.setStyle("-fx-text-fill: red");
+                // If the parsed expression is a CompoundExpression, then register some callbacks
+                if (rootExpression instanceof CompoundExpression) {
+                    ((Pane) rootExpression.getNode()).setBorder(Expression.NO_BORDER);
+                    final MouseEventHandler eventHandler = new MouseEventHandler();
+                    pane.setOnMousePressed(eventHandler);
+                    pane.setOnMouseDragged(eventHandler);
+                    pane.setOnMouseReleased(eventHandler);
                 }
+            } catch (ExpressionParseException epe) {
+                // If we can't parse the expression, then mark it in red
+                textField.setStyle("-fx-text-fill: red");
             }
         });
         queryPane.getChildren().add(button);
 
-
         // Reset the color to black whenever the user presses a key
+        // This is for resetting color in case an Expression failed to parse (and turned the text red)
         textField.setOnKeyPressed(e -> textField.setStyle("-fx-text-fill: black"));
 
         final BorderPane root = new BorderPane();
         root.setTop(queryPane);
-        root.setCenter(expressionPane);
+        root.setCenter(pane);
 
         primaryStage.setScene(new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT));
         primaryStage.show();
     }
 
+    /**
+     * Sets the border to red
+     *
+     * @param expToFind
+     */
+    private void setBorder(Expression expToFind) {
+        //TODO Work on this rootExpression
+        if (rootExpression instanceof LiteralExpression) {
+            expToFind.getNode().setStyle("-fx-border-style: solid inside;"
+                    + "-fx-border-width: 1;" + "-fx-border-insets: 2;"
+                    + "-fx-border-color: red;");
+        } else {
+            CompoundExpressionAb currentFocusCompound = (CompoundExpressionAb) rootExpression;
+            for (Expression expression : currentFocusCompound.getChildren()) {
+                if (expression == expToFind) {
+                    expression.getNode().setStyle("-fx-border-style: solid inside;"
+                            + "-fx-border-width: 1;" + "-fx-border-insets: 2;"
+                            + "-fx-border-color: red;");
+                } else expression.getNode().setStyle(null);
+            }
+        }
+    }
 }

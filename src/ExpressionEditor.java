5f766949be99a7cefe726fba446b4fcb1260f2aa
GabrielDeml <gabrielddeml@gmail.com>
2019-12-13 14:20:48 -0500

    TODO

--- src/ExpressionEditor.java
+++ src/ExpressionEditor.java
@@ -1,22 +1,15 @@
 import javafx.application.Application;
 
-import java.util.*;
-
-import javafx.geometry.Point2D;
-import javafx.scene.control.Label;
-import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.event.EventType;
+import javafx.geometry.Bounds;
+import javafx.geometry.Point2D;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
-import javafx.event.EventHandler;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class ExpressionEditor extends Application {
@@ -25,28 +18,102 @@ public class ExpressionEditor extends Application {
     }
 
     /**
+     * Currently focused Expression
+     */
+    //TODO this line
+    private Expression focusExpression;
+    /**
+     * Orig focus Expression
+     */
+    private Expression origExpression;
+
+    /**
      * Mouse event handler for the entire pane that constitutes the ExpressionEditor
      */
     private static class MouseEventHandler implements EventHandler<MouseEvent> {
+
+        /**
+         * rootExpression
+         */
+        CompoundExpression rootExpression;
+
+        /**
+         * pane
+         */
+        Pane pane;
+
         MouseEventHandler(Pane pane_, CompoundExpression rootExpression_) {
+            this.rootExpression = rootExpression_;
+            this.pane = pane_;
         }
 
-        double orgSceneX, orgSceneY;
+        /**
+         * Finds the clicked expression
+         *
+         * @param X location of the click
+         * @param Y location of the click
+         * @return The expression that was clicked or null if nothing was clicked
+         */
+        private Expression findClickedExpression(double X, double Y) {
+            CompoundExpressionAb currentFocusCompound = (CompoundExpressionAb) rootExpression;
+            for (Expression expression : currentFocusCompound.getChildren()) {
+                // Get the width and height of the hbox
+                double tmpWidth = expression.getNode().getLayoutBounds().getWidth();
+                double tmpHeight = expression.getNode().getLayoutBounds().getHeight();
+
+                Bounds boundsInScene = expression.getNode().localToScene(expression.getNode().getBoundsInLocal());
+//                Bounds tmpLocationX = expression.getNode().getBoundsInParent();
+//                double tmpLocationY = expression.getNode().getLayoutY();
+                // Location of the box in the scene
+                Point2D tmpSceneLocation = expression.getNode().localToScene(boundsInScene.getMinX(), boundsInScene.getMinY());
+                System.out.println("tmpSceneLocation.getX() :" + tmpSceneLocation.getX() + "\n tmpSceneLocation.getY() : " + tmpSceneLocation.getY() + "\n tmpLocationX : " + boundsInScene.getMinX() + "\n tmpLocationY : " + boundsInScene.getMinY() + "\n X : " + X + "\n Y : " + Y);
+                // Check if we are in those bounds
+                if (tmpSceneLocation.getX() < X && X < (tmpSceneLocation.getX() + tmpWidth) && tmpSceneLocation.getY() < Y && Y < (tmpSceneLocation.getY() + tmpHeight)) {
+                    return expression;
+                }
+            }
+            return null;
+        }
+        private void setBoarder(Expression expToFind){
+            CompoundExpressionAb currentFocusCompound = (CompoundExpressionAb) rootExpression;
+            for(Expression expression : currentFocusCompound.getChildren()){
+                if(expression == expToFind){
+                    expression.getNode().setStyle("-fx-border-style: solid inside;"
+                            + "-fx-border-width: 1;" + "-fx-border-insets: 2;"
+                            + "-fx-border-color: red;");
+                }else expression.getNode().setStyle(null);
+            }
+        }
+
+
+        double mouseLocationOnClickX, mouseLocationOnClickY;
         double orgTranslateX, orgTranslateY;
+
         public void handle(MouseEvent event) {
             if (event.getEventType() == MouseEvent.MOUSE_PRESSED) {
-                orgSceneX = event.getSceneX();
-                orgSceneY = event.getSceneY();
-                orgTranslateX = ((Pane) (event.getSource())).getTranslateX();
-                orgTranslateY = ((Pane) (event.getSource())).getTranslateY();
-            } else if (event.getEventType() == MouseEvent.MOUSE_DRAGGED) {
-                double offsetX = event.getSceneX() - orgSceneX;
-                double offsetY = event.getSceneY() - orgSceneY;
-                double newTranslateX = orgTranslateX + offsetX;
-                double newTranslateY = orgTranslateY + offsetY;
+                mouseLocationOnClickX = event.getSceneX();
+                mouseLocationOnClickY = event.getSceneY();
+                //TODO make this expression update line 24
+                Expression tmpExpression = findClickedExpression(mouseLocationOnClickX, mouseLocationOnClickY);
 
-                ((Pane) (event.getSource())).setTranslateX(newTranslateX);
-                ((Pane) (event.getSource())).setTranslateY(newTranslateY);
+                if (tmpExpression != null) {
+                    setBoarder(tmpExpression);
+//                    rootExpression = tmpExpression
+                    orgTranslateX = ((Pane) (event.getSource())).getTranslateX();
+                    orgTranslateY = ((Pane) (event.getSource())).getTranslateY();
+                }
+            } else if (event.getEventType() == MouseEvent.MOUSE_DRAGGED) {
+                mouseLocationOnClickX = event.getSceneX();
+                mouseLocationOnClickY = event.getSceneY();
+                Expression tmpExpression = findClickedExpression(mouseLocationOnClickX, mouseLocationOnClickY);
+                System.out.println("We are here");
+//                double offsetX = event.getSceneX() - mouseLocationOnClickX;
+//                double offsetY = event.getSceneY() - mouseLocationOnClickY;
+//                double newTranslateX = orgTranslateX + offsetX;
+//                double newTranslateY = orgTranslateY + offsetY;
+
+//                ((Pane) (event.getSource())).setTranslateX(newTranslateX);
+//                ((Pane) (event.getSource())).setTranslateY(newTranslateY);
             } else if (event.getEventType() == MouseEvent.MOUSE_RELEASED) {
                 //TODO implement me
             }
@@ -118,5 +185,13 @@ public class ExpressionEditor extends Application {
 
         primaryStage.setScene(new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT));
         primaryStage.show();
+    }
+
+    public Expression getFocusExpression() {
+        return focusExpression;
+    }
+
+    public Expression getOrigExpression() {
+        return origExpression;
     }
 }
